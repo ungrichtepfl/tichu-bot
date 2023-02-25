8856ebef26c21a45b9e9fe949f117f0da0f32241
Christoph Ungricht <christoph.ungricht@outlook.com>
2023-02-25 09:46:33 +0100

    feat: Restructure of game state. Added function to create initial game state.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Lib
+import Lib (playTichu)
 
 main :: IO ()
-main = someFunc
+main = playTichu
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,16 @@
 module Lib (
-  someFunc,
+  playTichu,
 ) where
 
-import Control.Monad.Random
+import Control.Monad
+import Data.Array.IO
 import System.Random
-import System.Random.TF
 
 data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
   deriving (Show, Eq, Ord, Enum, Bounded)
 
 data Color = Spades | Hearts | Diamonds | Clubs
-  deriving (Show, Eq, Enum, Bounded)
+  deriving (Show, Eq, Enum)
 
 data TichuCard = PokerCard (Value, Color) | Dragon | Phoenix | Mahjong | Dog
   deriving (Show, Eq)
@@ -18,34 +18,89 @@ data TichuCard = PokerCard (Value, Color) | Dragon | Phoenix | Mahjong | Dog
 type TichuCards = [TichuCard]
 
 type PlayerName = String
+
 type TeamName = String
 
 type Score = Int
 
-data Player = Player
-  { playerName :: PlayerName
-  , hand :: TichuCards
-  , trick :: TichuCards
+data GameConfig = GameConfig
+  { sittingOrder :: [PlayerName]
+  , teamNames :: [TeamName]
+  , scoreLimit :: Score
   }
   deriving (Show, Eq)
 
-data Team = Team
-  { players :: (Player, Player)
-  , teamName :: TeamName
-  , score :: Score
-  }
+playerNames :: GameConfig -> [PlayerName]
+playerNames = sittingOrder
+
+class Player player where
+  play :: player -> Game -> IO TichuAction
+
+newtype Distribution = Distribution [(PlayerName, [(PlayerName, TichuCard)])]
   deriving (Show, Eq)
 
-data GameRound = Dealing Int | Distributing Int | Playing Int | Counting
+data GameRound = Dealing PlayerName | Distributing Distribution | Playing [TichuAction] | Counting
+  deriving (Show, Eq)
+
+shuffle :: [a] -> IO [a]
+shuffle xs = do
+  ar <- newArray' n xs
+  forM [1 .. n] $ \i -> do
+    j <- randomRIO (i, n)
+    vi <- readArray ar i
+    vj <- readArray ar j
+    writeArray ar j vi
+    return vj
+ where
+  n = length xs
+  newArray' :: Int -> [a] -> IO (IOArray Int a)
+  newArray' n' = newListArray (1, n')
+
+data TichuAction = Play TichuCards | Pass | Tichu | GrandTichu
   deriving (Show, Eq)
 
 data Game = Game
-  { teams :: (Team, Team)
-  , round :: GameRound
+  { gameConfig :: GameConfig
+  , hands :: [(PlayerName, TichuCards)]
+  , tricks :: [(PlayerName, TichuCards)]
+  , tichuRound :: GameRound
+  , tichus :: [(PlayerName, Bool)]
   , deck :: TichuCards
-  , scoreLimit :: Int
   }
+  deriving (Show, Eq)
 
--- Random: https://stackoverflow.com/questions/59619690/generate-a-random-list-of-custom-data-type-where-values-provided-to-data-constr
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+orderedDeck :: TichuCards
+orderedDeck = [PokerCard (v, c) | v <- [Two .. Ace], c <- [Spades .. Clubs]] ++ [Dragon, Phoenix, Mahjong, Dog]
+
+shuffledDeck :: IO TichuCards
+shuffledDeck = shuffle orderedDeck
+
+initialHands :: [PlayerName] -> [(PlayerName, TichuCards)]
+initialHands names = [(n, []) | n <- names]
+
+initialTricks :: [PlayerName] -> [(PlayerName, TichuCards)]
+initialTricks names = [(n, []) | n <- names]
+
+initialTichus :: [PlayerName] -> [(PlayerName, Bool)]
+initialTichus names = [(n, False) | n <- names]
+
+initialGame :: GameConfig -> IO Game
+initialGame gameConfig = do
+  initialDeck <- shuffledDeck
+  shuffledPlayers <- shuffle $ playerNames gameConfig
+  let randomPlayer = head shuffledPlayers
+  let orderedPlayers = sittingOrder gameConfig
+  return $
+    Game
+      { gameConfig = gameConfig
+      , hands = initialHands orderedPlayers
+      , tricks = initialTricks orderedPlayers
+      , tichuRound = Dealing randomPlayer
+      , tichus = initialTichus orderedPlayers
+      , deck = initialDeck
+      }
+
+playTichu :: IO ()
+playTichu = do
+  game <- initialGame $ GameConfig ["Alice", "Bob", "Charlie", "David"] ["Team 1", "Team 2"] 1000
+  print game
