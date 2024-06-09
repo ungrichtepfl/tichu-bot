7838426e2d355a93c039b0ee58353eafae5bdfba
Christoph Ungricht <christoph.ungricht@outlook.com>
2024-06-09 22:27:30 +0200

    feat: Added raylib example
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,46 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main (main) where
 
-import Tichu (playTichu)
+-- import Tichu (playTichu)
+--
+-- main :: IO ()
+-- main = playTichu
 
-main :: IO ()
-main = playTichu
+import Raylib.Core (
+  clearBackground,
+  closeWindow,
+  initWindow,
+  setTargetFPS,
+  windowShouldClose,
+ )
+import Raylib.Core.Text (drawText)
+import Raylib.Util (
+  WindowResources,
+  drawing,
+  raylibApplication,
+ )
+import Raylib.Util.Colors (lightGray, rayWhite)
+
+startup :: IO WindowResources
+startup = do
+  window <- initWindow 600 450 "raylib [core] example - basic window"
+  setTargetFPS 60
+  return window
+
+mainLoop :: WindowResources -> IO WindowResources
+mainLoop window =
+  drawing
+    ( do
+        clearBackground rayWhite
+        drawText "Basic raylib window" 30 40 18 lightGray
+    )
+    >> return window
+
+shouldClose :: WindowResources -> IO Bool
+shouldClose _ = windowShouldClose
+
+teardown :: WindowResources -> IO ()
+teardown = closeWindow
+
+$(raylibApplication 'startup 'mainLoop 'shouldClose 'teardown)
