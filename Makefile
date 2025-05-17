bc330491430014fec5fccf670242114556daaf8b
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-05-16 21:05:38 -0400

    feat: Load assets for raylib gui.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- Makefile
+++ Makefile
@@ -26,3 +26,31 @@ wasm-run: wasm-build
 	cp "dist-newstyle/build/wasm32-wasi/ghc-$(ghc_wasm_version)/$(package)-$(pkg_version)/x/$(wasm_exe)/build/$(wasm_exe)/$(wasm_exe).wasm" $(wasm_dir)/
 	"$$(wasm32-wasi-ghc --print-libdir)"/post-link.mjs -i $(wasm_dir)/$(wasm_exe).wasm -o $(wasm_dir)/ghc_wasm_jsffi.js
 	python3 -m http.server --directory $(wasm_dir)
+
+CFLAGS:= -Wall -Werror -Wextra -Wpedantic -g
+CSRC:=gui/gui.c
+COUT_OBJ:=cout/gui.o
+COUT_EXE:=cout/gui
+COUT:=cout
+
+.PHONY: c-gui-build-obj
+c-gui-build-obj:
+	cc $(CFLAGS) $(CSRC) -c -o $(COUT_OBJ) -I./gui/raylib-5.0/linux_amd64/include -I/usr/include
+
+.PHONY: c-gui-build
+c-gui-build: c-gui-build-obj
+	cc $(CFLAGS) $(COUT_OBJ) -o $(COUT_EXE) -L./gui/raylib-5.0/linux_amd64/lib -l:libraylib.a -lm
+
+
+.PHONY: compiledb
+compiledb:
+	compiledb make c-gui-build
+
+EMCC_FLAGS := -sUSE_GLFW=3 -sUSE_LIBPNG -sASYNCIFY -sMODULARIZE=1 -sEXPORT_ES6=1 -sWASM=1 -sINITIAL_HEAP=256mb
+EMCC_FLAGS := $(EMCC_FLAGS) --embed-file ./Lato-Regular.ttf --embed-file ./trained_network.txt
+EMCC_FLAGS := $(EMCC_FLAGS) -sEXPORT_NAME=createDitect
+EMCC_FLAGS := $(EMCC_FLAGS) -sEXPORTED_FUNCTIONS=_run_gui,_send_mouse_button_down,_send_mouse_button_released,_send_space_pressed,_send_rkey_pressed
+
+.PHONY: wasm-gui-build
+wasm-c-gui-build:
+	exit 1 # TODO:
---
+++ gui/gui.c
@@ -0,0 +1,209 @@
+#include <assert.h>
+#include <raylib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#if defined(__EMSCRIPTEN__) || defined(__wasm__) || defined(__wasm32__) ||     \
+    defined(__wasm64__)
+#define WASM 1
+#else
+#define WASM 0
+#endif
+
+#define WIN_WIDTH 800
+#define WIN_HEIHT WIN_WIDTH
+#define FPS 60
+
+#define CARDS_PER_COLOR 13
+#define NUM_COLORS 4
+
+#define ASSET_BLACK_POSTFIX "a.png"
+#define ASSET_BLUE_POSTFIX "b.png"
+#define ASSET_GREEN_POSTFIX "c.png"
+#define ASSET_RED_POSTFIX "d.png"
+
+#define ASSET_DRAGON "drache.png"
+#define ASSET_DOG "hund.png"
+#define ASSET_MAHJONG "mahjong.png"
+#define ASSET_PHOENIX "phoenix.png"
+#define ASSET_BACKGROUND "background.png"
+
+#define ASSET_PATH "./gui/images/"
+#define CARD_ASSET_REL_PATH "cards/"
+
+typedef struct {
+  Texture2D red[CARDS_PER_COLOR];
+  Texture2D blue[CARDS_PER_COLOR];
+  Texture2D green[CARDS_PER_COLOR];
+  Texture2D black[CARDS_PER_COLOR];
+  Texture2D dragon;
+  Texture2D mahjong;
+  Texture2D phoenix;
+  Texture2D dog;
+  Texture2D background;
+} Assets;
+
+Assets g_assets = {0};
+
+void loadGlobalAssets(void) {
+
+  char asset_path[256];
+  Texture2D asset;
+  for (size_t i = 0; i < CARDS_PER_COLOR; ++i) {
+    char num[4];
+    sprintf(num, "%02zu", i + 2); // Assets starts with a 2
+
+    // BLACK ASSETS
+    sprintf(asset_path, "%s%s", ASSET_PATH CARD_ASSET_REL_PATH, num);
+    strncat(asset_path, ASSET_BLACK_POSTFIX, sizeof(asset_path) - 1);
+    asset = LoadTexture(asset_path);
+    g_assets.black[i] = asset;
+
+    // BLUE ASSETS
+    sprintf(asset_path, "%s%s", ASSET_PATH CARD_ASSET_REL_PATH, num);
+    strncat(asset_path, ASSET_BLUE_POSTFIX, sizeof(asset_path) - 1);
+    asset = LoadTexture(asset_path);
+    g_assets.blue[i] = asset;
+
+    // RED ASSETS
+    sprintf(asset_path, "%s%s", ASSET_PATH CARD_ASSET_REL_PATH, num);
+    strncat(asset_path, ASSET_RED_POSTFIX, sizeof(asset_path) - 1);
+    asset = LoadTexture(asset_path);
+    g_assets.red[i] = asset;
+
+    // GREEN ASSETS
+    sprintf(asset_path, "%s%s", ASSET_PATH CARD_ASSET_REL_PATH, num);
+    strncat(asset_path, ASSET_GREEN_POSTFIX, sizeof(asset_path) - 1);
+    asset = LoadTexture(asset_path);
+    g_assets.green[i] = asset;
+  }
+
+  // DRAGON
+  sprintf(asset_path, "%s", ASSET_PATH CARD_ASSET_REL_PATH);
+  strncat(asset_path, ASSET_DRAGON, sizeof(asset_path) - 1);
+  asset = LoadTexture(asset_path);
+  g_assets.dragon = asset;
+
+  // PHOENIX
+  sprintf(asset_path, "%s", ASSET_PATH CARD_ASSET_REL_PATH);
+  strncat(asset_path, ASSET_PHOENIX, sizeof(asset_path) - 1);
+  asset = LoadTexture(asset_path);
+  g_assets.phoenix = asset;
+
+  // DOG
+  sprintf(asset_path, "%s", ASSET_PATH CARD_ASSET_REL_PATH);
+  strncat(asset_path, ASSET_DOG, sizeof(asset_path) - 1);
+  asset = LoadTexture(asset_path);
+  g_assets.dog = asset;
+
+  // MAHJONG
+  sprintf(asset_path, "%s", ASSET_PATH CARD_ASSET_REL_PATH);
+  strncat(asset_path, ASSET_MAHJONG, sizeof(asset_path) - 1);
+  asset = LoadTexture(asset_path);
+  g_assets.mahjong = asset;
+
+  // BACKGROUND
+  sprintf(asset_path, "%s", ASSET_PATH);
+  strncat(asset_path, ASSET_BACKGROUND, sizeof(asset_path) - 1);
+  asset = LoadTexture(asset_path);
+  g_assets.background = asset;
+}
+
+void unloadGlobalAssets(void) {
+  for (size_t i = 0; i < CARDS_PER_COLOR; ++i) {
+    UnloadTexture(g_assets.green[i]);
+    UnloadTexture(g_assets.blue[i]);
+    UnloadTexture(g_assets.black[i]);
+    UnloadTexture(g_assets.red[i]);
+  }
+  UnloadTexture(g_assets.dragon);
+  UnloadTexture(g_assets.dog);
+  UnloadTexture(g_assets.mahjong);
+  UnloadTexture(g_assets.phoenix);
+  UnloadTexture(g_assets.background);
+}
+
+typedef enum {
+  RED_CARD,
+  GREEN_CARD,
+  BLUE_CARD,
+  BLACK_CARD,
+  DRAGON,
+  PHOENIX,
+  MAHJONG,
+  DOG,
+} CardColor;
+
+Texture2D getBackgroundAsset(void) { return g_assets.background; }
+
+Texture2D getCardAsset(CardColor color, int card_num) {
+  card_num -= 2; // Starts at 2
+  switch (color) {
+  case RED_CARD: {
+    assert(card_num < CARDS_PER_COLOR && "Wrong card number!");
+    return g_assets.red[card_num];
+  } break;
+  case BLUE_CARD: {
+    assert(card_num < CARDS_PER_COLOR && "Wrong card number!");
+    return g_assets.blue[card_num];
+  } break;
+  case BLACK_CARD: {
+    assert(card_num < CARDS_PER_COLOR && "Wrong card number!");
+    return g_assets.black[card_num];
+  } break;
+  case GREEN_CARD: {
+    assert(card_num < CARDS_PER_COLOR && "Wrong card number!");
+    return g_assets.green[card_num];
+  } break;
+  case DOG: {
+    return g_assets.dog;
+  } break;
+  case DRAGON: {
+    return g_assets.dragon;
+  } break;
+  case MAHJONG: {
+    return g_assets.mahjong;
+  } break;
+  case PHOENIX: {
+    return g_assets.phoenix;
+  } break;
+  default:
+    assert(0 && "Unreachable");
+  }
+}
+
+void init(void) {
+  InitWindow(WIN_WIDTH, WIN_HEIHT, "Tichu");
+  loadGlobalAssets();
+#if !WASM
+  SetTargetFPS(FPS);
+#endif
+}
+
+void deinit(void) {
+  unloadGlobalAssets();
+  CloseWindow();
+}
+
+void updateDraw(void) {
+
+  BeginDrawing();
+  ClearBackground(WHITE);
+  DrawTexture(getBackgroundAsset(), 0, 0, WHITE);
+  DrawTextureEx(getCardAsset(DRAGON, 0), (Vector2){400, 400}, 60, 1, WHITE);
+
+  EndDrawing();
+}
+
+#if !WASM
+int main(void) {
+  init();
+
+  while (!WindowShouldClose()) {
+    updateDraw();
+  }
+
+  deinit();
+}
+#endif
