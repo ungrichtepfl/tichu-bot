67c616e1751a80ec99f2ca096d70e4bf5a79c22d
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-08-29 20:36:34 +0800

    fix: Update test json.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- gui/main.c
+++ gui/main.c
@@ -4,19 +4,20 @@
 int main(void) {
   init(USER_PLAYER_INDEX);
 
-  parse_game_and_actions(&g_game_state, test_json1);
-  parse_game_and_actions(&g_game_state, test_json2);
+  parse_game_and_actions(&g_game_state, test_json);
 
   char game_config[MAX_BYTES_CONFIG_JSON] = {0};
   const char *game_json = NULL;
   STRBUFFCPY(game_config, "null");
   while (!window_should_close()) {
-    // if (strcmp(game_config, "null") == 0)
-    //   STRBUFFCPY(game_config, update_draw_config());
-    // else
-    game_json = update_draw_game(test_json1);
-    if (strcmp(game_json, "null") != 0) {
-      break;
+    if (strcmp(game_config, "null") == 0)
+      STRBUFFCPY(game_config, update_draw_config());
+    else {
+      update_c_state_and_render_game(test_json);
+      game_json = get_user_action();
+      if (strcmp(game_json, "null") != 0) {
+        break;
+      }
     }
   }
 
